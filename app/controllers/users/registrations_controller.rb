275740a5a987c4b837b173ec509cf4f93134c705
Hattleesa0 <isao.hattori@di-v.co.jp>
2020-05-23 14:44:35 +0900

    registration_users_table
    ## What
    usersテーブルの情報だけでユーザー登録
    
    ## Why
    users以外にもaddresses、sns_credentialsといったテーブルも使用するため。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
+  # def create    
   #   super
   # end
 
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # protected
-
+  private
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
@@ -64,12 +64,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
 end
