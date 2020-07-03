d9e2d59b35035415f68d30a34008ff9f5d2a58c7
Hattleesa0 <isao.hattori@di-v.co.jp>
2020-07-03 16:41:58 +0900

    商品画像編集、削除

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,10 +1,28 @@
 document.addEventListener('turbolinks:load', function () {
-
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
+
+  function buildImagePreview(blob_url, index) { //選択した画像ファイルのプレビューを表示する。
+    html = `
+            <div class="item-image new" data-index=${index}>
+              <img src =${blob_url} class="item-image__image">
+              <div class="item-image__buttons">
+                <div class="item-image__buttons--edit">
+                編集
+                </div>
+                <div class="item-image__buttons--delete">
+                削除
+                </div>
+              </div>
+            </div>
+            `;
+    return html;
+  }
+  /////////buildImagePreview()ここまで/////////
+
 
   function newFileField(index) { //新規画像投稿用のfile_fieldを作成しappendする。
     const html = `
-               <input accept="image/*" class="new-item-image" style="display: block;" data-index="${index}" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
+               <input accept="image/*" class="new-item-image" data-index="${index}" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
                `;
     return html;
   }
@@ -26,8 +44,18 @@ document.addEventListener('turbolinks:load', function () {
   $("#image-file-fields").on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
     const file = e.target.files[0];
     let index = $(this).data("index");
+    if (!file) {
+      const delete_button = $(`.item-image[data-index="${index}"]`).find(".item-image__buttons--delete");
+      delete_button.trigger("click");
+      return false;
+    }
     const blob_url = window.URL.createObjectURL(file); //選択された画像をblob url形式に変換する。
-    const preview_html = `<img src="${blob_url}" width="20%">`;
+    if ($(`.item-image[data-index="${index}"]`)[0]) {
+      const preview_image = $(`.item-image[data-index="${index}"]`).children("img");
+      preview_image.attr("src", blob_url);
+      return false;
+    }
+    const preview_html = buildImagePreview(blob_url, index);
     $("#select-image-button").before(preview_html);
     index += 1;
     const file_field_html = newFileField(index);
@@ -35,4 +63,23 @@ document.addEventListener('turbolinks:load', function () {
   });
   /////////file_fieldが変化した時ここまで/////////
 
+  /////////////////////////////////////////////
+  /////////画像の削除ボタンをクリックした時/////////
+  ////////////////////////////////////////////
+  $("#selected-item-images").on("click", ".item-image__buttons--delete", function (e) {
+    const index = $(this).parents(".item-image").data("index");
+    $(this).parents(".item-image").remove();
+    $(`#item_images_attributes_${index}__destroy`).prop("checked", true);
+    $(`#item_images_attributes_${index}_src`).remove();
+  });
+  /////////画像の削除ボタンをクリックした時ここまで/////////
+
+  /////////////////////////////////////////////
+  /////////画像の編集ボタンをクリックした時/////////
+  ////////////////////////////////////////////
+  $("#selected-item-images").on("click", ".item-image__buttons--edit", function (e) {
+    const index = $(this).parents(".item-image").data("index");
+    $(`#item_images_attributes_${index}_src`).trigger("click");
+  });
+  /////////画像の編集ボタンをクリックした時ここまで/////////
 });
